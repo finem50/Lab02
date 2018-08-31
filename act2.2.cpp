f5c325502d8c1f7917e5b90cf9918d4eccf1d8c4
Finem50 <finem@live.com>
2018-08-30 22:00:43 -0500

    Initial commit

---
+++ act2.2.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+using namespace std;
+
+int main(){
+
+  double age, ticket = 12;
+
+  cout << "Please enter the age \n";
+  cin >> age;
+  if(age<=8)
+    ticket=6;
+
+  cout << "Your ticket costs" << ticket << endl;
+
+  return 0;
+}
